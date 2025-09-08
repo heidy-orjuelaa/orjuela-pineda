f31ecbc2c3c6fec555946cdce710d05e1564384b
heidy-orjuelaa <alejitaramirez1916@gmail.com>
2025-09-07 22:32:06 -0500

    Primera Entrega del Proyecto

---
+++ SilkRoadCiclo1/skillroad-copy-copy-copy-copy/SilkRoad.java
@@ -0,0 +1,122 @@
+
+/**
+ * Write a description of class SilkRoad here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SilkRoad{
+
+    /**
+     * Crear un nuevo camino de longitud dada.
+     */
+    public SilkRoad(int length){
+
+    }
+
+    /**
+     * Coloca una tienda en una ubicación con cierta cantidad de tenges.
+     */
+    public void placeStore(int location, int tenges){
+        
+    }
+    
+    /**
+     * Elimina la tienda ubicada en la posición indicada.
+     */
+    public void removeStore(int location){
+        
+    }
+    
+    /**
+     * Coloca un robot en la ubicación indicada.
+     */
+    public void placeRobot(int location){
+        
+    }
+    
+    /**
+     * Elimina el robot ubicado en la posición indicada.
+     */
+    public void removeRobot(int location){
+        
+    }
+    
+    /**
+     * Mueve el robot desde una ubicación dada una cierta cantidad de metros.
+     */
+    public void moveRobot(int location, int meters){
+                
+    }
+    
+    /**
+     * Abastece todas las tiendas del camino.
+     */
+    public void resupplyStores(){
+        
+    }
+    
+    /**
+     * Hace que todos los robots regresen a su posición inicial.
+     */
+    public void returnRobots(){
+        
+    }
+    
+    /**
+     * Reinicia el sistema, devolviendo todo a su estado inicial.
+     */
+    public void reboot(){
+        
+    }
+    
+    /**
+     * Calcula y devuelve la ganancia total.
+     */
+    public int profit(){
+        
+    }
+    
+    /**
+     * Devuelve la información de todas las tiendas.
+     */
+    public int[][] stores(){
+        
+    }
+    
+    /**
+     * Devuelve la información de todos los robots.
+     */
+    public int[][] robots(){
+        
+    }
+    
+    /**
+     * Hace visible el camino y sus elementos gráficos.
+     */
+    public void makeVisible(){
+    
+    }
+    
+    /**
+     * Hace invisible el camino y sus elementos gráficos.
+     */
+    public void makeInvisible(){
+    
+    }
+    
+    /**
+     * Finaliza la simulación del camino.
+     */
+    public void finish(){
+    
+    }
+    
+    /**
+     * Verifica si el sistema está en un estado correcto.
+     */
+    public boolean ok(){
+    
+    }
+
+}
